64d288d6754c263c7a40db89caf1c1ab47089925
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-02-01 19:17:31 -0700

    recalculate canvas size everytime the window is resized

--- js/views/sixNimmtView.js
+++ js/views/sixNimmtView.js
@@ -6,11 +6,13 @@ class SixNimmtView {
 		this._handCanvas = $('#handCanvas')[0];
 		this._gameCtx = this._gameCanvas.getContext("2d");
 		this._handCtx = this._handCanvas.getContext("2d");
+		
 		this._resizeTimeout;
 		this.setCanvasSize();
-		$(window).resize(this.onResizeWindow.bind(this));	// i have to bind(this) because otherwise when onResizeWindow is called,
+		$(window).on("resize", this.onResizeWindow.bind(this));	// i have to bind(this) because otherwise when onResizeWindow is called,
 															// 'this' will be window, not this object, and it wont find setCanvasSize.
 															// event handlers are by default called with 'this' set to the window object
+		$(window).on("orientationchange", this.onResizeWindow.bind(this));
 	}
 	
 	setUpFlickity()
@@ -21,12 +23,31 @@ class SixNimmtView {
 	
 	setCanvasSize()
 	{
-		$(this._gallery).css("visibility", "hidden");
-		let widthOfGalleryCell = $($(this._gameCanvas).parents(".galleryCell")[0]).width();
-		this._gameCanvas.width = widthOfGalleryCell - 130;
-		this._handCanvas.width = widthOfGalleryCell - 130;
+		
+		const windowWidth = $(window).width();
+		const windowHeight = $(window).height();
+		const spaceForOneFlickityArrow = 65;
+		
+		let galleryWidth = windowWidth;
+		let canvasWidth = windowWidth - 2*spaceForOneFlickityArrow;
+		let canvasHeight = (16/24)*canvasWidth;
+		
+		if (canvasHeight > windowHeight)
+		{
+				canvasHeight = windowHeight*0.9;
+				canvasWidth = (21/16)*canvasHeight;
+				galleryWidth = canvasWidth + 2*spaceForOneFlickityArrow;
+		}
+		
+		$(this._gallery).css("width", galleryWidth+"px");
+		this._gameCanvas.width = canvasWidth;
+		this._gameCanvas.height = canvasHeight;
+		this._handCanvas.width = canvasWidth;
+		this._handCanvas.height = canvasHeight;
+
+		this._flickity.resize();	// the gallery sets its height to fit the tallest galleryCell. But you need to call resize for it to redraw.
 		console.log("Canvas resized...");
-		$(this._gallery).css("visibility", "visible");
+
 	}
 	
 	onResizeWindow()
