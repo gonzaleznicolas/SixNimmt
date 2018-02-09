9bc5dd41072d8f61efdbbe4a4313d2be32161f20
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-02-08 20:44:21 -0700

    when the card gets really narrow nothing crosses over anymore

--- js/views/basicShapeDrawer.js
+++ js/views/basicShapeDrawer.js
@@ -4,16 +4,21 @@ class BasicShapeDrawer
 {
 	static drawCardShape(ctx, x, y, width, height, radius)
 	{
-			ctx.beginPath();
-			ctx.moveTo(x, y + radius);
-			ctx.lineTo(x, y + height - radius);
-			ctx.arcTo(x, y + height, x + radius, y + height, radius);
-			ctx.lineTo(x + width - radius, y + height);
-			ctx.arcTo(x + width, y + height, x + width, y + height-radius, radius);
-			ctx.lineTo(x + width, y + radius);
-			ctx.arcTo(x + width, y, x + width - radius, y, radius);
-			ctx.lineTo(x + radius, y);
-			ctx.arcTo(x, y, x, y + radius, radius);
+			if (width > 15)
+			{
+				ctx.beginPath();
+				ctx.moveTo(x, y + radius);
+				ctx.lineTo(x, y + height - radius);
+				ctx.arcTo(x, y + height, x + radius, y + height, radius);
+				ctx.lineTo(x + width - radius, y + height);
+				ctx.arcTo(x + width, y + height, x + width, y + height-radius, radius);
+				ctx.lineTo(x + width, y + radius);
+				ctx.arcTo(x + width, y, x + width - radius, y, radius);
+				ctx.lineTo(x + radius, y);
+				ctx.arcTo(x, y, x, y + radius, radius);
+			}
+			else
+				ctx.rect(x, y, width, height);
 	}
 
 	static drawDetailedCowShape(ctx, centreX, centreY, cowWidth, cowHeight)
