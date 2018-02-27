bf1c7083f88818e4c7cc10c68a70d02f083ca19e
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-02-26 17:34:08 -0700

    adding menu background in js rather than html

--- js/views/menuView.js
+++ js/views/menuView.js
@@ -5,7 +5,8 @@ class MenuView
 	constructor()
 	{
 		this._menuElement = $('#menu');
-		this._menuBackground = $('#menuBackground');
+		this._menuBackground = $(document.createElement("div")).addClass("menuBackground");
+		$('header').after(this._menuBackground);
 		this._menuButton = $("#menuButton");
 		
 		this._menuButton[0].addEventListener("click", this.menuButtonClicked.bind(this), false);
