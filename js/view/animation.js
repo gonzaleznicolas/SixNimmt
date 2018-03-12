2b98e425dce59f7c519f4bd6818bc91c962d0306
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-03-12 16:19:08 -0600

    moved fadeAwayCard into the parent animation class

--- js/view/animation.js
+++ js/view/animation.js
@@ -7,4 +7,28 @@ class Animation
 		this._drawer = drawer;
 		this._model = model;
 	}
+	
+	fadeAwayCard(row, col)
+	{
+		bAnimationInProgress = true;
+		this._fadeIteration = 0;
+		this._fadeX = this._drawer._cardCoordinates[row][col].x;
+		this._fadeY = this._drawer._cardCoordinates[row][col].y;
+		requestAnimationFrame(this.fadeAwayCardHelper.bind(this));
+	}
+	
+	fadeAwayCardHelper()
+	{
+		if (this._fadeIteration < 30)
+		{
+			this._drawer.dimCard(this._fadeX, this._fadeY, 0.1);
+			requestAnimationFrame(this.fadeAwayCardHelper.bind(this));
+			this._fadeIteration++;
+		}
+		else
+		{
+			this._drawer.clearCardSpace(this._fadeX, this._fadeY);
+			bAnimationInProgress = false;
+		}
+	}
 }
--- js/view/handAnimation.js
+++ js/view/handAnimation.js
@@ -6,28 +6,4 @@ class HandAnimation extends Animation
 	{
 		super(new HandDrawer($('#handCanvas')[0], model), model);
 	}
-	
-	fadeAwayCard(row, col)
-	{
-		bAnimationInProgress = true;
-		this._fadeIteration = 0;
-		this._fadeX = this._drawer._cardCoordinates[row][col].x;
-		this._fadeY = this._drawer._cardCoordinates[row][col].y;
-		requestAnimationFrame(this.fadeAwayCardHelper.bind(this));
-	}
-	
-	fadeAwayCardHelper()
-	{
-		if (this._fadeIteration < 30)
-		{
-			this._drawer.dimCard(this._fadeX, this._fadeY, 0.1);
-			requestAnimationFrame(this.fadeAwayCardHelper.bind(this));
-			this._fadeIteration++;
-		}
-		else
-		{
-			this._drawer.clearCardSpace(this._fadeX, this._fadeY);
-			bAnimationInProgress = false;
-		}
-	}
 }
