7b6d9ce3d4b5e10eb4855f7e8b5957c54b487f26
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-03-16 15:28:54 -0600

    wait animation done

--- js/startup/wait.js
+++ js/startup/wait.js
@@ -1,102 +1,11 @@
 "use strict";
 
+
 $(function () {
-		drawCow();
-		$('#english')[0].addEventListener("click", onEnglish, false);
-		$('#espanol')[0].addEventListener("click", onSpanish, false);
-		$('#newGame')[0].addEventListener("click", onNewGame, false);
-		$('#vsAI')[0].addEventListener("click", onVsAI, false);
-		$('#joinGame')[0].addEventListener("click", onJoinGame, false);
-		$('#spectateGame')[0].addEventListener("click", onSpectateGame, false);
+ drawCow();
+ fadeCow();
+ animateDots();
 });
-
-function onEnglish() {
-	$('#videoInstructions')[0].innerHTML = "Video Instructions";
-	$('#textInstructions')[0].innerHTML = "Text Instructions";
-
-	$('#newGame')[0].innerHTML = "New Game";
-	$('#vsAI')[0].innerHTML = "1v1 vs Computer";
-	$('#joinGame')[0].innerHTML = "Join Game";
-	$('#spectateGame')[0].innerHTML = "Spectate Game";
-
-	$('#nickNamePrompt')[0].innerHTML = "Nickname for yourself:";
-	$('#codePrompt')[0].innerHTML = "Game code:";
-
-	$('#nickNameError')[0].innerHTML = "That nickname is taken by someone in the game you are trying to join. Try again.";
-	$('#codeError')[0].innerHTML = "That code does not identify an open game. Try again.";
-
-	$('#nickNameTextBox').attr("placeholder", "max 6 characters");
-	$('#codeTextBox').attr("placeholder", "4 digit code");
-
-	languageClicked();
-}
-
-function onSpanish() {
-	$('#videoInstructions')[0].innerHTML = "Video Instrucciones";
-	$('#textInstructions')[0].innerHTML = "Instrucciones Escritas";
-
-	$('#newGame')[0].innerHTML = "Iniciar nuevo juego";
-	$('#vsAI')[0].innerHTML = "Jugar contra computador";
-	$('#joinGame')[0].innerHTML = "Unirse a juego";
-	$('#spectateGame')[0].innerHTML = "Ver juego";
-
-	$('#nickNamePrompt')[0].innerHTML = "Tu apodo:";
-	$('#codePrompt')[0].innerHTML = "Codigo del juego:";
-
-	$('#nickNameError')[0].innerHTML = "Ese apodo esta ocupado. Intenta otro.";
-	$('#codeError')[0].innerHTML = "Ese codigo no corresponde a un juego activo.";
-
-	$('#nickNameTextBox').attr("placeholder", "max 6 caracteres");
-	$('#codeTextBox').attr("placeholder", "codigo de 4 digitos");
-	languageClicked();
-}
-
-function languageClicked() {
-	$('#instructionsSection').css("visibility", "visible");
-	$('#gameOptionsSection').css("visibility", "visible");
-}
-
-function onNewGame() {
-		$('#gameOptionsSection').children().removeClass("selected");
-		$('#newGame').addClass("selected");
-
-		$('#form').children().hide();
-		$('#nickName').show();
-		$('#inputSection').css("visibility", "visible");
-}
-
-function onVsAI() {
-		$('#gameOptionsSection').children().removeClass("selected");
-		$('#vsAI').addClass("selected");
-
-		$('#form').children().hide();
-		$('#inputSection').css("visibility", "hidden");
-}
-
-function onJoinGame() {
-		$('#gameOptionsSection').children().removeClass("selected");
-		$('#joinGame').addClass("selected");
-
-		$('#form').children().hide();
-		$('#nickName').show();
-		$('#code').show();
-
-		// these 3 lines are temporary to show how to show errors
-		$("#nickNameStatus").attr("src", "img/check.png").css("visibility", "visible");
-		$("#codeStatus").attr("src", "img/x.png").css("visibility", "visible");
-		$("#codeError").show();
-
-		$('#inputSection').css("visibility", "visible");
-}
-
-function onSpectateGame() {
-		$('#gameOptionsSection').children().removeClass("selected");
-		$('#spectateGame').addClass("selected");
-
-		$('#form').children().hide();
-		$('#code').show();
-		$('#inputSection').css("visibility", "visible");
-}
 
 function drawCow()
 {
@@ -109,4 +18,23 @@ function drawCow()
 	ctx.stroke();
 	ctx.fill();
 	ctx.closePath();
+}
+
+function fadeCow()
+{
+ $("#cow").fadeTo(2000, 0.3, function () { $("#cow").fadeTo(2000, 1, fadeCow) })
+}
+
+
+let nextDotIndex = 0;
+let animationSpeed = 400;
+function animateDots()
+{
+ nextDotIndex = (nextDotIndex + 1) % $("#dots").children().length;
+ $("#dots").children().eq(nextDotIndex).hide(animationSpeed, function () { $("#dots").children().eq(nextDotIndex).show(animationSpeed, animateDots); })
+}
+
+function addPlayer()
+{
+ $("#dots").append("<div></div>");
 }
