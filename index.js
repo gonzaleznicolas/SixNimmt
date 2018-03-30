20f8c12a8ca2452ff4a46e5f887aec3b43c173a8
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-03-30 17:26:41 -0600

    changing spaces for tabs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('public'));
 app.get('/', function(req, res){
-    res.sendFile( __dirname + "/" + "home.html" );
+	res.sendFile( __dirname + "/" + "home.html" );
 });
 
 app.listen(80);
