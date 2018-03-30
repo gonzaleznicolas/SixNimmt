a6f7e2a015749a74994bc9ed7f2851b98b007f34
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-03-30 17:00:11 -0600

    set up node pt 2

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
 app.get('/', function(req, res){
-   res.send("Hello world nico!");
+    res.sendFile( __dirname + "/" + "home.html" );
 });
 
 app.listen(80);
