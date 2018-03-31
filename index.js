6e58275749e71c39f23f3a2d4079403a52b4cb86
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-03-30 23:36:17 -0600

    identify which type of form was submitten

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ app.get('/', function(req, res){
 	res.sendFile( __dirname + "/" + "home.html" );
 });
 
+const FormType = Object.freeze({ "NewGame": 1, "vsAI": 2, "JoinGame": 3, "SpectateGame": 4 })
 app.post('/process_form', function (req, res) {
-	
+	let formType = req.body.formType;
+	if (formType == FormType.NewGame)
+	{
+		console.log(formType);
+	}
+	else if (formType == FormType.vsAI)
+	{
+		console.log(formType);
+	}
+	else if (formType == FormType.JoinGame)
+	{
+		console.log(formType);
+	}
+	else // formType == FormType.SpectateGame
+	{
+		console.log(formType);
+	}
  })
 
 http.listen(80, function() {
