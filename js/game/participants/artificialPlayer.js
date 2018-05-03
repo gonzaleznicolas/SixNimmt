5c78194e832c0c01e0d607931baefa389da75f77
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-05-02 19:14:26 -0600

    renamed a bunch of variables

--- js/game/participants/artificialPlayer.js
+++ js/game/participants/artificialPlayer.js
@@ -73,14 +73,17 @@ module.exports = class ArtificialPlayer extends Player
 				continue;
 			let lastCardOnRowI = this.tableAtStartOfThisRound.lastCardInRow(rowI);
 
-			let listOfCardsThatWouldGoBeforeMine = [];
+			// "killer" cards are cards that would go on the same row that myCard would go on,
+			// but they would go first (they are smaller)
+
+			let listOfKillerCards = [];
 			for (let c = lastCardOnRowI + 1; c < myCardForRowI; c++)
-				listOfCardsThatWouldGoBeforeMine.push(c);
+				listOfKillerCards.push(c);
 			
-			// remove from listOfCardsThatWouldGoBeforeMine any card that has already been played.
+			// remove from listOfKillerCards any card that has already been played.
 			// i.e. any cards that i know no other player can play
-			let listOfCardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard =
-				listOfCardsThatWouldGoBeforeMine.filter( function(card) {
+			let listOfKillerCardsThtCouldBePlayedThisRound =
+				listOfKillerCards.filter( function(card) {
 					 return !this._setOfCardsIveSeenAlready.has(card);
 				}.bind(this));
 			
@@ -91,7 +94,7 @@ module.exports = class ArtificialPlayer extends Player
 			let nCowsIdTakeIfMineIsThe6th = 10;
 
 			pMyCardWillBeThe6th = this.calculatePMyCardWillBe6th(
-				listOfCardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard.length,
+				listOfKillerCardsThtCouldBePlayedThisRound.length,
 				nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th
 			);
 
@@ -105,72 +108,71 @@ module.exports = class ArtificialPlayer extends Player
 		this.emit('playerPlayCard', {player: this, playedCard: cardToPlay});
 	}
 
-	calculatePMyCardWillBe6th(nCardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard, nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th)
+	calculatePMyCardWillBe6th(nKillerCardsThtCouldBePlayedThisRound, nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th)
 	{
 		let pMyCardWillBeThe6th;
 		let nPlayersOtherThanMe = this._totalNumberOfPlayersInGameImInIncludingMyself - 1;
-		let max_n_PlayersWhoCanHaveACardThatWouldGoBeforeMine = 
-			Math.min(nCardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard,
+		let max_n_PlayersWhoCanHaveAKillerCard = 
+			Math.min(nKillerCardsThtCouldBePlayedThisRound,
 				nPlayersOtherThanMe );
-		if (max_n_PlayersWhoCanHaveACardThatWouldGoBeforeMine
-			< nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th)
+
+		// in order for my card to be the 6th, there needs to be at least nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th different players
+		// in the game, all of whom have a killer card,
+		// and exacly nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th need to play that card this round
+
+		// find pMyCardWillBeThe6th iteratively by considering the different number of players who can have killer cards
+
+		pMyCardWillBeThe6th = 0;
+
+		// i is the number of players who have a killer card
+		for (let nPWHKK = nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th;
+			nPWHKK <= max_n_PlayersWhoCanHaveAKillerCard;
+			nPWHKK++)
 		{
-			pMyCardWillBeThe6th = 0;
-		}
-		else
-		{
-			// there are enough CardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard and enough players 
-			// so that they could all play cards making mine the 6th
+			// calculate the probability that exactly nPWHKK players have a card that would go before mine.
+			let p_nPWHLL_playersHaveKillerCard = this.calc_p_H_playersHaveKillerCard(
+				104 - this._setOfCardsIveSeenAlready.size,
+				nKillerCardsThtCouldBePlayedThisRound,
+				this._hand.Size,
+				nPlayersOtherThanMe,
+				nPWHKK
+			);
 
-			// in order for my card to be the 6th, there needs to be at least nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th different players
-			// in the game, all of whom have a card in their hand in listOfCardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard,
-			// and exacly nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th need to play that card this turn
+			// calculate the probability that exactly nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th players
+			// out of the nPWHKK who have a killer card play the killer card that they have
 
-			pMyCardWillBeThe6th = 0;
+			let pAGivenPlayerPlaysTheCard = 0.8;
+			let p_exactly_necessaryPlayersToMakeMine6thPlayTheirKillerCard = this.p_outOf_N_Players_K_ChooseCard(
+				nPWHKK,
+				nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th,
+				pAGivenPlayerPlaysTheCard
+			);
 
-			// i is is the number of players who have a card that would go before mine
-			for (let i = nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th;
-				i <= max_n_PlayersWhoCanHaveACardThatWouldGoBeforeMine;
-				i++)
-			{
-				// calculate the probability that exactly i players have a card that would go before mine.
-				let p_exactly_i_playersHaveCardThatWouldGoBeforeMine = pExactlyHPlayersHaveCardThatGoesBeforeMine(
-					104 - this._setOfCardsIveSeenAlready.size,
-					nCardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard,
-					this._hand.Size,
-					nPlayersOtherThanMe,
-					i
-				);
+			// a is the probability that nPWHKK players have killer card, and exactly nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th
+			// play it
+			let a = p_nPWHLL_playersHaveKillerCard*p_exactly_necessaryPlayersToMakeMine6thPlayTheirKillerCard;
 
-				if (p_exactly_i_playersHaveCardThatWouldGoBeforeMine > 1)
-				{
-					console.log("error: this probability cant be > 1");
-					console.log(this._setOfCardsIveSeenAlready.size);
-					console.log(nCardsThatCouldBePlayedThisTurnThatWouldGoOnRowIBeforeMyCard);
-					console.log(this._hand.Size);
-					console.log(nPlayersOtherThanMe);
-					console.log(i);
-					//throw "error"
-				}
-
-				// calculate the probability that exactly nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th players
-				// play the card they have which would go before mine
-
-				// nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th <= i
-				// i players have a card that would go before mine
-				// what is the probability that nuCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th play it
-				let pAGivenPlayerPlaysTheCard = 0.8;
-				let p_exactly_necessaryPlayersToMakeMine6thPlayRightCard = p_K_outOf_N_PlayersChooseCard(
-					i,
-					nCardsThatWouldHaveToBePlacedBeforeMineToMakeMineThe6th,
-					pAGivenPlayerPlaysTheCard
-				);
-
-				pMyCardWillBeThe6th += (p_exactly_i_playersHaveCardThatWouldGoBeforeMine*p_exactly_necessaryPlayersToMakeMine6thPlayRightCard);
-			}
+			pMyCardWillBeThe6th += a;
 		}
 
 		return pMyCardWillBeThe6th;
+	}
+
+	calc_p_H_playersHaveKillerCard(
+		nCardsWhichIHaveNotSeen, // this includes all the cards that were not used for this iteration. (eg if there are only 2 players, each is delt 10, 4 are placed on the table, so only 24/104 cards are used)
+		nCardsWhichWouldGoOnThisRowBeforeMine,
+		nCardsLeftInEachPlayersHand,
+		nPlayersOtherThanMe,
+		h
+	)
+	{
+		return 0.5;
+	}
+
+	p_outOf_N_Players_K_ChooseCard(N, K, pAPlayerWillChooseCard)
+	{
+		let p = pAPlayerWillChooseCard;
+		return C(N, K) * Math.pow(p, K) * Math.pow(1-p, N-K);
 	}
 
 	// METHODS CALLED BY THE GAME. METHODS ANY PLAYER MUST IMPLEMENT
@@ -253,70 +255,4 @@ function factorial(n){
 function C(n, r)
 {
 	return Math.round(factorial(n)/(factorial(r)*factorial(n-r)));
-}
-
-function p_K_outOf_N_PlayersChooseCard(N, K, pAPlayerWillChooseCard)
-{
-	let p = pAPlayerWillChooseCard;
-	return C(N, K) * Math.pow(p, K) * Math.pow(1-p, N-K);
-}
-
-function pExactlyHPlayersHaveCardThatGoesBeforeMine(
-	nCardsWhichIHaveNotSeen, // this includes all the cards that were not used for this iteration. (eg if there are only 2 players, each is delt 10, 4 are placed on the table, so only 24/104 cards are used)
-	nCardsWhichWouldGoOnThisRowBeforeMine,
-	nCardsLeftInEachPlayersHand,
-	nPlayersOtherThanMe,
-	h
-)
-{
-	let nBallsInTheBagOriginally = nCardsWhichIHaveNotSeen;
-	let nRedBallsOriginally = nCardsWhichWouldGoOnThisRowBeforeMine;
-	let nBlueBallsOriginally = nBallsInTheBagOriginally - nRedBallsOriginally;
-	let nBallsInEachBin = nCardsLeftInEachPlayersHand;
-	let nTotalBins = nPlayersOtherThanMe;
-	let nBinsWithAtLeastOneRed = h;
-	let nBinsWithAllBlue = nTotalBins - nBinsWithAtLeastOneRed;
-
-	let nBallsLeftInBag = nBallsInTheBagOriginally;
-	let nRedBallsLeftInBag = nRedBallsOriginally;
-	let nBlueBallsLeftInBag = nBlueBallsOriginally;
-
-	let nWaysToFillBinsFollowingRestriction = 1;
-
-	// put a red ball in each of the bins which require at least one red
-	for (let binWithRedBall = 0; binWithRedBall < nBinsWithAtLeastOneRed; binWithRedBall++)
-	{
-		let nBallsToPutInTheBin = 1;
-		nWaysToFillBinsFollowingRestriction *= C(nRedBallsLeftInBag, nBallsToPutInTheBin);
-		nBallsLeftInBag -= nBallsToPutInTheBin;
-		nRedBallsLeftInBag -= nBallsToPutInTheBin;
-	}
-
-	// fill the other bins with blue balls
-	for (let binWithAllBlue = 0; binWithAllBlue < nBinsWithAllBlue; binWithAllBlue++)
-	{
-		let nBallsToPutInTheBin = nBallsInEachBin;
-		nWaysToFillBinsFollowingRestriction *= C(nBlueBallsLeftInBag, nBallsToPutInTheBin);
-		nBallsLeftInBag -= nBallsToPutInTheBin;
-		nBlueBallsLeftInBag -= nBallsToPutInTheBin;
-	}
-
-	// finish filling the bins where you put a red with whatever is left in the bag. color doesnt matter anymore
-	for (let binWithRedBall = 0; binWithRedBall < nBinsWithAtLeastOneRed; binWithRedBall++)
-	{
-		let nBallsToPutInTheBin = nBallsInEachBin - 1;
-		nWaysToFillBinsFollowingRestriction *= C(nBallsLeftInBag, nBallsToPutInTheBin);
-		nBallsLeftInBag -= nBallsToPutInTheBin;
-	}
-
-	nBallsLeftInBag = nBallsInTheBagOriginally;
-	let totalnWaysToFillTheBinsDisregardingColor = 1;
-	for (let bin = 0; bin < nTotalBins; bin ++)
-	{
-		let nBallsToPutInTheBin = nBallsInEachBin;
-		totalnWaysToFillTheBinsDisregardingColor *= C(nBallsLeftInBag, nBallsToPutInTheBin);
-		nBallsLeftInBag -= nBallsToPutInTheBin;
-	}
-
-	return nWaysToFillBinsFollowingRestriction/totalnWaysToFillTheBinsDisregardingColor;
 }
