0cf4387b5735adaf51f0ed8ac5f9c93d529be5d9
Nicolas Gonzalez <nicolas.g96@gmail.com>
2018-05-06 09:35:50 -0600

    change font

--- public/js/view/drawer.js
+++ public/js/view/drawer.js
@@ -159,7 +159,7 @@ class Drawer
 		const ctx = this._ctx;
 
 		const fontPixels = 0.5*this._cardHeight;
-		ctx.font = "bold "+fontPixels+"px Do Hyeon";
+		ctx.font = "bold "+fontPixels+"px Boogaloo";
 		ctx.textAlign = "center";
 		ctx.textBaseline = "middle";
 		const centreXofNumber= x + (cardWidth/2);
