99ae5263cbda9ed761ff5aca5863d26f3adf0e24
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-04-02 08:42:36 -0600

    organize html files

---
+++ public/js/gameLog.js
@@ -0,0 +1,12 @@
+$(function () {
+	new gridjs.Grid({
+		columns: ["Name", "Email", "Phone Number"],
+		data: [
+			["John", "johnf@example.com", "(353) 01 222 3333"],
+			["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+			["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+			["Sarah", "sarahcdd@gmmail.com", "+322 876 1233"],
+			["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+		]
+		}).render(document.getElementById("gridWrapper"));
+});
